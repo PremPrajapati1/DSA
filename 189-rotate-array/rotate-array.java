c0c11cf695d510453a78d4704cdf1584ab4a0fa4
Prem <129924071+PremPrajapati1@users.noreply.github.com>
2026-08-30 21:33:36 +0530

    Time: 8 ms (5.45%) | Memory: 264.1 MB (21.11%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,22 +1,38 @@
 class Solution {
-    public void rev(int[] nums, int start, int end){
-        while(start <= end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+    // public void rev(int[] nums, int start, int end){
+    //     while(start <= end){
+    //         int temp = nums[start];
+    //         nums[start] = nums[end];
+    //         nums[end] = temp;
 
-            start = start + 1;
-            end = end - 1;
-        }
-    }
+    //         start = start + 1;
+    //         end = end - 1;
+    //     }
+    // }
     public void rotate(int[] nums, int k) {
-        if( k % nums.length == 0){
-            return;
-        }
-        k = k % nums.length;
+        // if( k % nums.length == 0){
+        //     return;
+        // }
+        // k = k % nums.length;
 
-        rev(nums, 0, nums.length - 1);
-        rev(nums, 0, k-1);
-        rev(nums, k, nums.length - 1);
+        // rev(nums, 0, nums.length - 1);
+        // rev(nums, 0, k-1);
+        // rev(nums, k, nums.length - 1);
+
+        // Second Approach
+        int count = 0;
+
+        for(int i = 0; count < nums.length; i++){
+            int idx = i;
+            int curr = nums[idx];
+
+            do {
+                int next = nums[(idx + k) % nums.length];
+                nums[(idx + k) % nums.length] = curr;
+                curr = next;
+                idx = (idx + k) % nums.length;
+                count = count + 1;
+            } while(idx != i);
+        }
     }
 }
